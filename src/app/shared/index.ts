9f3aa63c9322207c6d064e6806edf056bbc733ad
Scott Lau <liuweigeek@outlook.com>
2020-09-21 14:21:59 +0800

    optimize global search

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,7 +1,5 @@
 export * from './service';
 export * from './entity';
-/*export * from './directive';
-export * from './component';*/
 export * from './interceptor';
 export * from './guard';
 export * from './shared.module';
