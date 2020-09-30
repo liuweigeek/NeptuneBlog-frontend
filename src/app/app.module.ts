a777a73ab6068cf8c6ec2c079691f712ac267632
Scott Lau <liuweigeek@outlook.com>
2020-09-30 17:00:42 +0800

    optimize the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
-import { AppRoutingModule } from './app-routing.module';
-
 import { NZ_I18N, zh_CN } from 'ng-zorro-antd/i18n';
+import zh from '@angular/common/locales/zh';
+
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzMessageModule } from 'ng-zorro-antd/message';
 
-import { AppComponent } from './app.component';
 import { AuthInterceptor } from './shared/interceptor';
+import { AppComponent } from './app.component';
 import { HomeModule } from './home';
 import { AuthenticationModule } from './authentication';
 
