9607782eb11f57f32211fe7d224018efc5030c6d
Scott Lau <liuweigeek@outlook.com>
2021-06-07 18:32:41 +0800

    optimized the imports

--- src/app/authentication/service/authentication.service.ts
+++ src/app/authentication/service/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { User } from '../../shared/entity';
+import { User } from '../../shared';
 import { Observable } from 'rxjs';
 import { timeout } from 'rxjs/operators';
 
